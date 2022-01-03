6a6d01ae00a7a597d4d174025560fd77f6575538
Julien Roncaglia <julien@roncaglia.fr>
2022-01-03 17:58:21 +0100

    Integer example ++

--- examples/integer.rs
+++ examples/integer.rs
@@ -4,4 +4,6 @@ fn main() {
     let r: Range<_> = (1..=5).into();
 
     println!("Range: {:?}", r);
+    println!("Contains 4: {}", r.contains(4));
+    println!("Contains 10: {}", r.contains(10));
 }
