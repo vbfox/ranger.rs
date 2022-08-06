618df9fec684bceae592fb9a712c3d148057b450
Julien Roncaglia <julien@roncaglia.fr>
2022-08-07 00:35:57 +0200

    Add a script for coverage

---
+++ coverage.ps1
@@ -0,0 +1,29 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+function ThrowOnNativeFailure {
+    if (-not $?)
+    {
+        throw 'Native Failure'
+    }
+}
+
+Remove-Item 'target' -Recurse
+
+# Export the flags needed to instrument the program to collect code coverage.
+$env:RUSTFLAGS = "-Cinstrument-coverage"
+
+# Ensure each test runs gets its own profile information by defining the LLVM_PROFILE_FILE environment variable (%p will be replaced by the process ID, and %m by the binary signature):
+$env:LLVM_PROFILE_FILE = "target/coverage/ranger-%p-%m.profraw"
+
+# Build & test
+cargo build
+ThrowOnNativeFailure
+
+cargo test
+ThrowOnNativeFailure
+
+# Generate a HTML report in the coverage/ directory.
+grcov ./target/coverage/ --binary-path ./target/debug/ -s ./src/ -t html --branch --ignore-not-existing -o ./coverage/
+ThrowOnNativeFailure
+
+Invoke-Item -Path 'coverage/index.html'
